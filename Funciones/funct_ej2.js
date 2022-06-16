f5f53de020e6661c0b9aa344ac0f9d006692c57a
Julio Hernández <julioergocesar@outlook.com>
2022-06-15 21:29:27 -0500

    Se corrigieron errores en funct_ej2

--- Funciones/funct_ej2.js
+++ Funciones/funct_ej2.js
@@ -6,29 +6,30 @@ hacer login incremente este valor.
 En la función principal se pedirá un nombre de usuario y una contraseña y se intente hacer login,
  solamente tenemos tres oportunidades para intentarlo.*/
 
-function Login(user, password, contador) {
-    if (user=="usuario1"&&password=="asdasd"){
-      return true;
-    }
-    contador++
-    return false
+ function Login(user, password) {
+  if (user=="usuario1"&&password=="asdasd"){
+    return true;
   }
-  
-  function Principal(){
-    let intentos
-    intentos=0
-  while (intentos<=3){
-    let usuario=prompt("Ingrese su usuario: ")
-    let contraseña=prompt("Ingrese su contraseña: ")
-    let respuesta=confirm(`¿Desea acceder? Ha hecho ${intentos} intentos`)
-    if (respuesta==true){
-      if (Login(usuario, contraseña,intentos)){
-        alert(`Bienvenido ${usuario}`)
-        break;
-      } 
-      alert("Revise su usuario y contraseña")
-  }
-  }
-  }
-  Principal()
+  return false
+}
+
+function Principal(){
+  let intentos
+  intentos=0
+while (intentos<3){
+  let usuario=prompt("Ingrese su usuario: ")
+  let contraseña=prompt("Ingrese su contraseña: ")
+  let respuesta=confirm(`¿Desea acceder? Ha hecho ${intentos} intentos`)
+  if (respuesta==true){
+    if (Login(usuario, contraseña,intentos)){
+      alert(`Bienvenido ${usuario}`)
+      break;
+    } 
+    intentos++
+    alert("Revise su usuario y contraseña")
+}
+}
+}
+Principal()
+
   
